6374b44d22921c3a8df66aa6c0287c7bfc52d373
yasmeenbsharat <y.bsharat@student.aaup.edu>
2023-10-08 11:24:22 +0300

    Add generatedata function

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,12 @@
 document.querySelector('form').onsubmit = (e) => {
     e.preventDefault();
   }
+  let searchValue;
 const searchInput = document.getElementById("search");
 const searchButton = document.getElementById("searchButton");
 const resultsContents = document.getElementById("results");
 const apiUrl="https://en.wikipedia.org/w/api.php?action=query&list=search&prop=info&inprop=url&utf8=&format=json&origin=*&srlimit=20&srsearch=";
-let results ;
+// let results ;
 let error="";
 
 function debounce(func, delay) {
@@ -17,32 +18,32 @@ function debounce(func, delay) {
 }
 
 function search() {
-   const searchValue=searchInput.value;
+    searchValue=searchInput.value;
     if(searchValue == "") {
         error='Search can not be empty .Please enter a search term  :)';
         errorMessage();
     }else {
-    getResults(searchValue);
+   generateData (getResults,displayResults);
     }
    
 }
-async function getResults(searchValue){
+async function getResults(){
     const request = await fetch(apiUrl+searchValue);
-    results  = await request.json();
+     let results  = await request.json();
     results=results.query.search;
     if(results.length==0){
       error='No Search results :)';
       errorMessage();
     }
     else{
-        displayResults();
+       return results;
     }}
 
-function displayResults(){
+function displayResults(results){
     let data='';
     results.map((result)=>{
         const resultUrl=`https://en.wikipedia.org/?curid= ${result.pageid}`;
-        data +=`  <div class="result p-2">
+        data +=`  <div class="result p-3 border-1 border-secondary rounded-2 border mb-4">
         <a href="${resultUrl}" target="_blank" class="fs-3 fw-bold">
             ${result.title}
         </a>
@@ -59,6 +60,10 @@ function displayResults(){
 function errorMessage(){
    resultsContents.innerHTML = `<div class="alert alert-danger w-50 m-auto " > ${error}</div>`;
 }
+async function generateData (callback,display){
+    const data=await callback();
+    display(data);
 
+ }
 const debouncedSearch = debounce(search, 500);
 searchButton.addEventListener('click', debouncedSearch);
